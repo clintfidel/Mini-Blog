be828b5ca487913744fa688c51b2ddda65bd5861
clintfidel <clintfidel@gmail.com>
2018-01-10 01:30:20 +0100

    import articles route to server

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import winston from 'winston';
 import validator from 'express-validator';
-// import BlogRoutes from './server/routes/recipe'
- import UserRoutes from './server/routes/user'
+import BlogRoutes from './server/routes/article';
+import UserRoutes from './server/routes/user';
 
 const app = express();
 
 dotenv.load();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 
- app.use('/api/v1/users', UserRoutes)
-// app.use('/api/v1/recipes', BlogRoutes)
+app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/articles', BlogRoutes);
 
 app.get('*', (req, res) => {
-  res.send('welcome to My Blog App')
-})
+  res.send('welcome to My Blog App');
+});
 
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5050;
 
 app.listen(port, () => {
-  winston.info(`Connected on port ${port}`)
+  winston.info(`Connected on port ${port}`);
 });
