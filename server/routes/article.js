ae1e62b37131c347a79d6c8a14cff0eef2bdbccc
clintfidel <clintfidel@gmail.com>
2018-01-11 13:27:38 +0100

    create endpoint/route for editArticle

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import articles from '../controllers/articles';
 import { isLoggedIn } from '../middlewares/Authorization';
-import { checkArticleInput,
+import { checkArticleInput, validateEditUserId,
   verifyUserIdExist, verifyBlogIdExist,
   blogTitleExist } from '../middlewares/Validation';
 
-const { create, deleteArticle } = articles;
+const { create, deleteArticle, editArticle } = articles;
 const app = express.Router();
 
 app.route('/')
@@ -16,5 +16,11 @@ app.route('/')
 
 app.route('/delete/:blogId')
   .delete(isLoggedIn, verifyUserIdExist, verifyBlogIdExist, deleteArticle);
+
+app.route('/edit/:blogId')
+  .put(
+    isLoggedIn, verifyUserIdExist, verifyBlogIdExist,
+    blogTitleExist, validateEditUserId, editArticle
+  );
 
 export default app;
