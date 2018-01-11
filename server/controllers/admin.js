a5ebaf779d5b08c2fdf54f91af8d9b73be4ed48a
clintfidel <clintfidel@gmail.com>
2018-01-11 15:54:25 +0100

    feat: authenticated admin should be able to edit articles added by users

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -25,6 +25,38 @@ const adminController = {
       });
   },
 
+  adminEditArticle(req, res) {
+    const { id } = req.decoded.currentUser;
+    Blog
+      .update(req.body, {
+        where: {
+          id: +req.params.blogId,
+        }
+      })
+      .then((edit) => {
+        if (!edit) {
+          res.status(404).send({
+            message: 'No Blog found for edit'
+          });
+        }
+
+        return res.status(200).json({
+          message: 'Blog sucessfully updated',
+          data: {
+            blogTitle: req.body.blogTitle,
+            blogPost: req.body.blogPost,
+            rate: req.body.rate,
+            blogId: req.params.blogId,
+            userId: id
+          }
+        });
+      })
+
+      .catch((error) => {
+				console.log(error, '====>')
+        res.status(500).send('Internal sever Error');
+      });
+  },
 };
 
 export default adminController;
