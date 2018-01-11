b59e9ac92d1aaa80577f6bc31ef031e0d9070f30
clintfidel <clintfidel@gmail.com>
2018-01-11 15:55:14 +0100

    create endpoint/route to enable admin edit users articles

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,12 +4,12 @@ import adminAction from '../controllers/admin';
 import { isLoggedIn, isAdmin } from '../middlewares/Authorization';
 import { checkArticleInput, validateEditUserId,
   verifyUserIdExist, verifyBlogIdExist,
-  blogTitleExist } from '../middlewares/Validation';
+  blogTitleExist, verifyBlogIdParams } from '../middlewares/Validation';
 
 const {
   create, deleteArticle, editArticle, getAllArticles
 } = articles;
-const { adminDeleteArticle } = adminAction;
+const { adminDeleteArticle, adminEditArticle } = adminAction;
 const app = express.Router();
 
 app.route('/')
@@ -20,15 +20,27 @@ app.route('/')
   .get(isLoggedIn, getAllArticles);
 
 app.route('/delete/:blogId')
-  .delete(isLoggedIn, verifyUserIdExist, verifyBlogIdExist, deleteArticle);
+  .delete(
+    isLoggedIn, verifyBlogIdParams, verifyUserIdExist, verifyBlogIdExist,
+    deleteArticle
+  );
 
 app.route('/edit/:blogId')
   .put(
     isLoggedIn, verifyUserIdExist, verifyBlogIdExist,
-    blogTitleExist, validateEditUserId, editArticle
+    verifyBlogIdParams, blogTitleExist, validateEditUserId, editArticle
   );
 
 app.route('/admin/delete/:blogId')
-  .delete(isLoggedIn, isAdmin, verifyBlogIdExist, adminDeleteArticle);
+  .delete(
+    isLoggedIn, isAdmin, verifyBlogIdExist,
+    verifyBlogIdParams, adminDeleteArticle
+  );
+
+app.route('/admin/edit/:blogId')
+  .put(
+    isLoggedIn, isAdmin, verifyBlogIdExist, verifyBlogIdParams,
+    validateEditUserId, blogTitleExist, adminEditArticle
+  );
 
 export default app;
