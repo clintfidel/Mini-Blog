e8a2de5bf91beeda0e1a1543f0a6e27395832463
clintfidel <clintfidel@gmail.com>
2018-01-11 02:19:13 +0100

    feat: create endpoint/route to enable user delete blog

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,16 +1,20 @@
 import express from 'express';
-import article from '../controllers/articles';
+import articles from '../controllers/articles';
 import { isLoggedIn } from '../middlewares/Authorization';
 import { checkArticleInput,
-  verifyUserIdExist, blogTitleExist } from '../middlewares/Validation';
+  verifyUserIdExist, verifyBlogIdExist,
+  blogTitleExist } from '../middlewares/Validation';
 
+const { create, deleteArticle } = articles;
 const app = express.Router();
 
 app.route('/')
   .post(
     isLoggedIn, checkArticleInput,
-    blogTitleExist, verifyUserIdExist, article.create
+    blogTitleExist, verifyUserIdExist, create
   );
 
+app.route('/delete/:blogId')
+  .delete(isLoggedIn, verifyUserIdExist, verifyBlogIdExist, deleteArticle);
 
 export default app;
