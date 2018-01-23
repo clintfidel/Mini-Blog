047867e5f167ca391f963cb0957b01132e3cedd4
clintfidel <clintfidel@gmail.com>
2018-01-23 01:22:30 +0100

    feature: implement signup amd login reducers

---
+++ client/reducers/AuthReducer.js
@@ -0,0 +1,33 @@
+import { SIGNUP_ERROR, SIGNUP_USER, SIGNIN_USER } from '../actions/types';
+
+const initialState = {
+  user: {},
+  authenticated: false,
+  error: ''
+};
+
+/**
+ * @description - User authentication reducer
+ *
+ * @param {Object} state - Default application state
+ *
+ * @param {Object} action - Response from the API
+ *
+ * @returns {Object} - Object containing new state
+ */
+function AuthReducer(state = initialState, action) {
+  switch (action.type) {
+    case SIGNUP_USER:
+      return { ...state, user: action.user, authenticated: true };
+
+    case SIGNIN_USER:
+      return { ...state, user: action.user, authenticated: true };
+
+    case SIGNUP_ERROR:
+      return { ...state, user: action.message, authenticated: false };
+
+    default:
+      return state;
+  }
+}
+export default AuthReducer;
