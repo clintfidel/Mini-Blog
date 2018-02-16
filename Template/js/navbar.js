67e8f9ed5786a0cdbd0088b643d0bee7dcda6a72
clintfidel <clintfidel@gmail.com>
2018-02-16 09:25:00 +0100

    add toggle for comment box

--- Template/js/navbar.js
+++ Template/js/navbar.js
@@ -1,9 +1,15 @@
 $('.modal').on('shown.bs.modal', () => {
-  $('.all-articles').css({ opacity: 0.1 });
+  $('.all-articles, .container-comment').css({ opacity: 0.1 });
 });
 
 // when modal closes
 $('.modal').on('hidden.bs.modal', () => {
-  $('.all-articles').css({ opacity: 1 });
+  $('.all-articles, .container-comment').css({ opacity: 1 });
 });
 
+$(document).ready(() => {
+  $('.material-button-toggle').on('click', function () {
+    $(this).toggleClass('open');
+    $('.option').toggleClass('scale-on');
+  });
+});
