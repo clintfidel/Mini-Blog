4370493db7a40139f2bfb07380213a3d6d5cdd24
clintfidel <clintfidel@gmail.com>
2018-02-17 00:06:07 +0100

    bug: user should not sign up or login with empty fields

--- client/utils/validation.js
+++ client/utils/validation.js
@@ -1,5 +1,5 @@
 
-export const checkUserInput = (firstInput, secondInput, thirdInput) => {
+const checkUserInput = (firstInput, secondInput, thirdInput) => {
   if (firstInput.trim() === '' || secondInput.trim() === ''
   || thirdInput.trim() === '') {
     return true;
@@ -7,19 +7,4 @@ export const checkUserInput = (firstInput, secondInput, thirdInput) => {
   return false;
 };
 
-
-export const isInValidField = (fieldData) => {
-  if (typeof (fieldData) !== 'string' || fieldData.length === 0) {
-    return true;
-  }
-  let hasValue = false;
-  fieldData.split().forEach((value) => {
-    if (value !== ' ') {
-      hasValue = true;
-    }
-  });
-  if (!hasValue) {
-    return true;
-  }
-  return false;
-};
+export default checkUserInput;
