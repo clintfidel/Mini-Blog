057a8c3f0ae8154315c0aac7d5054a603efb8eb3
clintfidel <clintfidel@gmail.com>
2018-02-16 23:57:48 +0100

    feature: import login and signup components  for signin and sign up

---
+++ client/components/presentational/includes/NavBar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+import Login from '../../container/auth/LoginForm'
+import SignUp from '../../container/auth/SignupForm'
+
+class NavBar extends Component {
+  render () {
+    return (
+      <div>
+        <div className='all-page'>
+          <nav className='my_navbar'>
+            <div className='container-fluid'>
+              <div className='navbar-header'>
+                <button
+                  type='button'
+                  className='navbar-toggle collapsed'
+                  data-toggle='collapse'
+                  data-target='#myNavbar'
+                  aria-expanded='false'
+                  aria-controls='myNavbar'>
+                  <span className='sr-only'>Toggle navigation</span>
+                  <span className='icon-bar'></span>
+                  <span className='icon-bar'></span>
+                  <span className='icon-bar'></span>
+                </button>
+                <a className='navbar-brand' href='./navbar.html'>Clint's Blog... <i className='fa fa-pencil' aria-hidden='true' style={{fontSize:'40px'}}></i></a>
+              </div>
+              <div className='collapse navbar-collapse' id='myNavbar'>
+                <ul className='nav navbar-nav navbar-right'>
+                  <li className=''>
+                    <a href='#' data-toggle='modal' data-target='#modalLoginForm'>Login</a>
+                    <Login />
+                  </li>
+                  <li>
+                    <a href='#' data-toggle='modal' data-target='#modalSignUpForm'>Register</a>
+                    <SignUp/>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    )
+  }
+}
+
+// NavBar.propTypes = {
+
+// }
+
+export default NavBar
