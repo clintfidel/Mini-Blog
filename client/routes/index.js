ed8c6f4000ec179a6ce8809da859684bd6c2cdba
clintfidel <clintfidel@gmail.com>
2018-02-17 10:18:19 +0100

    create routing for homepage

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import SignupAndSignin from '../components/presentational/includes/NavBar';
-// import AllBlog from '../components/container/ArticlesPage';
+import User from '../components/presentational/includes/NavBar';
+import HomePage from '../components/container/pages/Homepage'
 
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route path='/createUser' component={SignupAndSignin} />
+      <Route path='/' component={HomePage} />
+      <Route path='/user' component={User} />
       
     </Switch>
   </Router>
