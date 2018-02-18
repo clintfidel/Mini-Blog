438bd74baec47ba41df744866c531685aa9989c4
clintfidel <clintfidel@gmail.com>
2018-02-18 23:09:12 +0100

    create action type for all-articles

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,3 +1,4 @@
 export const SIGNUP_USER = 'SIGNUP_USER';
 export const SIGNIN_USER = 'SIGNIN_USER';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const GET_ALL_ARTICLES = 'GET_ALL_ARTICLES';
