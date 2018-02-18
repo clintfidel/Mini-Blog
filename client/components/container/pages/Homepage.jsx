e42a2b97dd96dc61bd9267a291e83cda83eb2428
clintfidel <clintfidel@gmail.com>
2018-02-18 23:12:42 +0100

    connect component to redux store to render all articles dynamically

--- client/components/container/pages/Homepage.jsx
+++ client/components/container/pages/Homepage.jsx
@@ -1,21 +1,94 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import Articles from '../../presentational/pages/Articles'
-import NavBar from '../../presentational/includes/NavBar'
-import Footer from '../../presentational/includes/Footer'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../../../public/css/style.css';
+import Articles from '../../presentational/pages/Articles';
+import NavBar from '../../presentational/includes/NavBar';
+import Footer from '../../presentational/includes/Footer';
+import getAllArticles from '../../../actions/ArticlesAction';
 
-const HomePage = (props) => (
-  <div>
-    <div className='all-page'>
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+     this.state = {
+       loader: false
+     }
+     this.renderAllArticles=this.renderAllArticles.bind(this);
+  }
+
+  componentDidMount() {
+    $('.modal').on('shown.bs.modal', () => {
+      $('.all-articles, .container-comment').css({ opacity: 0.1 });
+    });
+    
+    // when modal closes
+    $('.modal').on('hidden.bs.modal', () => {
+      $('.all-articles, .container-comment').css({ opacity: 1 });
+    });
+    
+    $(document).ready(() => {
+      $('.material-button-toggle').on('click', function () {
+        $(this).toggleClass('open');
+        $('.option').toggleClass('scale-on');
+      });
+    });
+
+    this.setState({
+      loader: true
+    });
+    this.props.getAllArticles()
+      .then(() => {
+        this.setState({
+          loader: false
+        });
+      });
+  }
+
+  renderAllArticles() {
+    let allArticles = this.props.articles;
+    console.log(allArticles)
+    if (allArticles.length < 1){
+      return <div className= 'notifier'><h1>No article found </h1></div>
+    }
+
+    else {
+      return allArticles.map((article) =>{
+        console.log(article)
+        return (
+          <Articles 
+          title ={article.blogTitle}
+          post = {article.blogPost}
+          views ={article.views}
+          id={article.id}
+          key={article.id}
+          />
+        )
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <div className='all-page'>
       <NavBar />
-      <Articles />
+      <div className= 'row'>
+      {this.renderAllArticles()}
+      </div>
       <Footer />
     </div>
-  </div>
-)
+      </div>
+    );
+  }
+}
 // HomePage.propTypes = {
 
 // }
+const mapStateToProps = (state) => {
+  console.log(state, '====>');
+  return {
+    articles: state.ArticleReducer.articles
+  };
+}
 
-export default HomePage
+export default connect(mapStateToProps, {getAllArticles}) (HomePage);
