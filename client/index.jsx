2800e75401794399191a731c81c8c56a602319a0
clintfidel <clintfidel@gmail.com>
2018-02-18 23:16:10 +0100

    import bootstrap from node modules to display proper styling

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
 // import './public/js/index';
+import 'bootstrap/dist/js/bootstrap'; // for responsive navbar dropdown and modal dialog
+import 'bootstrap/dist/css/bootstrap.css';
+// import './public/css/style.css'
 
 import configureStore from './store/configureStore';
 import Routes from './routes/index';
