529d10661d833cda27101a6ce1114aaa9d586d41
clintfidel <clintfidel@gmail.com>
2018-02-18 23:13:44 +0100

    modify articlepage component

--- client/components/presentational/pages/Articles.jsx
+++ client/components/presentational/pages/Articles.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const ArticlesList = () => {
+const ArticlesList = ({title, post}) => {
   return (
     <div>
       <main className='all-articles'>
@@ -16,10 +16,9 @@ const ArticlesList = () => {
                   </a>
                 </div>
                 <div className='card-body text-left'>
-                  <h4 className='card-title'><strong>About me</strong></h4>
+                  <h4 className='card-title'><strong>{title}</strong></h4>
                   <p className='card-text'>
-                    Lorem ipsum dolor sit amet, consectetur sunt elit. Impedit iste perspiciatis earum voluptatem ad possimus itaque, eum aspernatur, nostrum dignissimos beatae id in
-                    illum accusantium adipisicing suscipit sunt doloremque.
+                   {post}
                   </p>
                   <a href='#'><span className='fa fa-star checked'></span></a>
                   <a href='#'><span className='fa fa-star checked'></span></a>
@@ -30,136 +29,6 @@ const ArticlesList = () => {
                     <p> 0 comments </p>
                   </a>
                   <a href='./comment.html' className='btn btn-deep-orange btn-md'>Read more</a>
-                </div>
-              </div>
-            </div>
-            <div className='col-lg-4 col-md-6 mb-4'>
-              <div className='card'>
-                <div className='view overlay hm-white-slight'>
-                  <img src='https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (134).jpg' className='img-fluid' alt='photo' />
-                  <a href='#!'>
-                    <div className='mask'></div>
-                  </a>
-                </div>
-                <div className='card-body text-left'>
-                  <h4 className='card-title indigo-text'><strong>About me</strong></h4>
-                  <p className='card-text'>
-                    Sed ut perspiciatis unde omnis iste natus error volosit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab eum illo inventore veritatis
-                    quasi architecto beatae vitae dicta explicabo.
-                  </p>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a> |
-                  <a href='#'>
-                    <p> 0 comments </p>
-                  </a>
-                  <a href='./comment.html' className='btn btn-indigo btn-md'>Learn more</a>
-                </div>
-              </div>
-            </div>
-            <div className='col-lg-4 col-md-6 mb-4'>
-              <div className='card'>
-                <div className='view overlay hm-white-slight'>
-                  <img src='https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (135).jpg' className='img-fluid' alt='photo' />
-                  <a href='#!'>
-                    <div className='mask'></div>
-                  </a>
-                </div>
-                <div className='card-body text-left'>
-                  <h4 className='card-title font-up brown-text'><strong>About me</strong></h4>
-                  <p className='card-text black-text'>
-                    Nemo enim ipsam voluptatem, consectetur sunt elit. Impedit iste perspiciatis earum voluptatem ad possimus itaque, eum aspernatur, nostrum dignissimos beatae id in
-                    illum accusantium adipisicing suscipit sunt doloremque.
-                  </p>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a> |
-                  <a href='#'>
-                    <p> 0 comments </p>
-                  </a>
-                  <a href='./comment.html' className='btn btn-brown btn-md'>More content</a>
-                </div>
-              </div>
-            </div>
-            <div className='col-lg-4 col-md-6 mb-4'>
-              <div className='card'>
-                <div className='view overlay hm-white-slight'>
-                  <img src='https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (135).jpg' className='img-fluid' alt='photo' />
-                  <a href='#!'>
-                    <div className='mask'></div>
-                  </a>
-                </div>
-                <div className='card-body text-left'>
-                  <h4 className='card-title font-up brown-text'><strong>About me</strong></h4>
-                  <p className='card-text black-text'>
-                    Nemo enim ipsam voluptatem, consectetur sunt elit. Impedit iste perspiciatis earum voluptatem ad possimus itaque, eum aspernatur, nostrum dignissimos beatae id in
-                    illum accusantium adipisicing suscipit sunt doloremque.
-                  </p>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a> |
-                  <a href='#'>
-                    <p> 0 comments </p>
-                  </a>
-                  <a href='./comment.html' className='btn btn-brown btn-md'>More content</a>
-                </div>
-              </div>
-            </div>
-            <div className='col-lg-4 col-md-6 mb-4'>
-              <div className='card'>
-                <div className='view overlay hm-white-slight'>
-                  <img src='https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (135).jpg' className='img-fluid' alt='photo' />
-                  <a href='#!'>
-                    <div className='mask'></div>
-                  </a>
-                </div>
-                <div className='card-body text-left'>
-                  <h4 className='card-title font-up brown-text'><strong>About me</strong></h4>
-                  <p className='card-text black-text'>
-                    Nemo enim ipsam voluptatem, consectetur sunt elit. Impedit iste perspiciatis earum voluptatem ad possimus itaque, eum aspernatur, nostrum dignissimos beatae id in
-                    illum accusantium adipisicing suscipit sunt doloremque.
-                  </p>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a> |
-                  <a href='#'>
-                    <p> 0 comments </p>
-                  </a>
-                  <a href='./comment.html' className='btn btn-brown btn-md'>More content</a>
-                </div>
-              </div>
-            </div>
-            <div className='col-lg-4 col-md-6 mb-4'>
-              <div className='card'>
-                <div className='view overlay hm-white-slight'>
-                  <img src='https://mdbootstrap.com/img/Photos/Horizontal/People/6-col/img (134).jpg' className='img-fluid' alt='photo' />
-                  <a href='#!'>
-                    <div className='mask'></div>
-                  </a>
-                </div>
-                <div className='card-body text-left'>
-                  <h4 className='card-title indigo-text'><strong>About me</strong></h4>
-                  <p className='card-text'>
-                    Sed ut perspiciatis unde omnis iste natus error volosit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab eum illo inventore veritatis
-                    quasi architecto beatae vitae dicta explicabo.
-                  </p>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a>
-                  <a href='#'><span className='fa fa-star checked'></span></a> |
-                  <a href='#'>
-                    <p> 0 comments </p>
-                  </a>
-                  <a href='./comment.html' className='btn btn-indigo btn-md'>Learn more</a>
                 </div>
               </div>
             </div>
