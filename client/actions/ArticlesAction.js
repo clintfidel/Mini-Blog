2edfdf6ddde73eb304ee4cb67a880edde841e906
clintfidel <clintfidel@gmail.com>
2018-02-18 23:18:25 +0100

    feature: create article action to get all articles

---
+++ client/actions/ArticlesAction.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { GET_ALL_ARTICLES } from './types';
+
+const apiUrl = 'api/v1/articles/';
+const getAllArticles = dispatch => axios
+  .get(apiUrl)
+  .then((response) => {
+    dispatch({
+      type: GET_ALL_ARTICLES,
+      allArticles: response.data
+    });
+  })
+  .catch(error => error.response);
+
+export default getAllArticles;
