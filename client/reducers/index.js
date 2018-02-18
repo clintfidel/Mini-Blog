17f87e55e1583023621c5f20cb99b2df423f14e6
clintfidel <clintfidel@gmail.com>
2018-02-18 23:17:47 +0100

    add articleReducer to state

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import AuthReducer from './AuthReducer';
+import ArticleReducer from './ArticlesReducer';
 
 
 const rootReducer = combineReducers({
-  AuthReducer
+  AuthReducer,
+  ArticleReducer
 });
 
 export default rootReducer;
