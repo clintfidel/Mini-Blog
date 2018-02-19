449ab8552ec589ea4948f2a5c80b668b507df991
clintfidel <clintfidel@gmail.com>
2018-02-19 21:47:02 +0100

    feature:create user logout action

--- client/actions/AuthAction.js
+++ client/actions/AuthAction.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
-import setAuthorization from '../utils/Authorization';
-// import toastrOption from '../utils/toastrOption';
-import { SIGNIN_USER, SIGNUP_USER } from './types';
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+import setAuthorization from '../utils/Authorization'
+ import toastrOption from '../utils/toastrOption';
+import { SIGNIN_USER, SIGNUP_USER, UNAUTH_USER } from './types'
 
-
-// toastrOption();
+// toastrOption()
 /**
  * @description - Set current signed up user
  *
@@ -13,11 +12,11 @@ import { SIGNIN_USER, SIGNUP_USER } from './types';
  *
  * @returns {Object} - redux action to be dispatched
  */
-export function signup(user) {
+export function signup (user) {
   return {
     type: SIGNUP_USER,
     user
-  };
+  }
 }
 /**
  * @description - Set current signed up user
@@ -26,32 +25,47 @@ export function signup(user) {
  *
  * @returns {Object} - redux action to be dispatched
  */
-export function signin(user) {
+export function signin (user) {
   return {
     type: SIGNIN_USER,
     user
-  };
+  }
 }
 
 export const registerAction = userDetails => dispatch => axios
   .post('/api/v1/users/signup', userDetails)
   .then((response) => {
-    const { token } = response.data;
-    setAuthorization(token);
-    localStorage.setItem('token', token);
-    const currentUser = jwtDecode(token);
-    dispatch(signup(currentUser));
+    const { token } = response.data
+    setAuthorization(token)
+    localStorage.setItem('token', token)
+    const currentUser = jwtDecode(token)
+    dispatch(signup(currentUser))
+    $('.modal').on('shown.bs.modal', function () {
+      $('.all-articles').hide()
+    })
   })
-  .catch(error =>
-    Promise.reject(error.response.data.message));
+  .catch(error => Promise.reject(error.response.data.message))
 
 export const loginAction = userDetails => dispatch => axios
   .post('/api/v1/users/signin', userDetails)
   .then((response) => {
-    const { token } = response.data;
-    setAuthorization(token);
-    localStorage.setItem('token', token);
-    const currentUser = jwtDecode(token);
-    dispatch(signin(currentUser));
+    const { token } = response.data
+    setAuthorization(token)
+    localStorage.setItem('token', token)
+    const currentUser = jwtDecode(token)
+    dispatch(signin(currentUser))
+    $('.modal-backdrop.fade').css('opacity', 0)
   })
-  .catch(error => Promise.reject(error.response.data.message));
+  .catch(error => Promise.reject(error.response.data.message))
+
+  export const logoutAction = () => (dispatch) => {
+    localStorage.removeItem('jwtToken')
+    
+      setAuthorization(false);
+      dispatch({
+        type: UNAUTH_USER,
+        user: {}
+      })
+      toastrOption();
+      toastr.success('You have logged out successfully'); 
+  }
