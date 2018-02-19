7755279b1fdb7d37634f8782f438949e39188cf2
clintfidel <clintfidel@gmail.com>
2018-02-19 21:48:04 +0100

    action type for user logout

--- client/actions/types.js
+++ client/actions/types.js
@@ -1,4 +1,6 @@
 export const SIGNUP_USER = 'SIGNUP_USER';
 export const SIGNIN_USER = 'SIGNIN_USER';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const UNAUTH_USER = 'UNAUTH_USER';
 export const GET_ALL_ARTICLES = 'GET_ALL_ARTICLES';
+export const ADD_ARTICLE = 'ADD_ARTICLE'
