e9a567634ecf46c4bb198068db54bfa5c4e08654
clintfidel <clintfidel@gmail.com>
2018-02-19 05:24:52 +0100

    fix display of articles

--- client/components/container/pages/Homepage.jsx
+++ client/components/container/pages/Homepage.jsx
@@ -1,83 +1,84 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import '../../../public/css/style.css';
-import Articles from '../../presentational/pages/Articles';
-import NavBar from '../../presentational/includes/NavBar';
-import Footer from '../../presentational/includes/Footer';
-import getAllArticles from '../../../actions/ArticlesAction';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import '../../../public/css/style.css'
+import Articles from '../../presentational/pages/Articles'
+import NavBar from '../../presentational/includes/NavBar'
+import Footer from '../../presentational/includes/Footer'
+import getAllArticles from '../../../actions/ArticlesAction'
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-     this.state = {
-       loader: false
-     }
-     this.renderAllArticles=this.renderAllArticles.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = {
+      loader: false
+    }
+    this.renderAllArticles = this.renderAllArticles.bind(this)
   }
 
-  componentDidMount() {
+  componentDidMount () {
     $('.modal').on('shown.bs.modal', () => {
-      $('.all-articles, .container-comment').css({ opacity: 0.1 });
-    });
-    
+      $('.all-articles, .container-comment').css({ opacity: 0.1 })
+    })
+
     // when modal closes
     $('.modal').on('hidden.bs.modal', () => {
-      $('.all-articles, .container-comment').css({ opacity: 1 });
-    });
-    
+      $('.all-articles, .container-comment').css({ opacity: 1 })
+    })
+
     $(document).ready(() => {
-      $('.material-button-toggle').on('click', function () {
-        $(this).toggleClass('open');
-        $('.option').toggleClass('scale-on');
-      });
-    });
+      $('.material-button-toggle').on('click', () => {
+        $(this).toggleClass('open')
+        $('.option').toggleClass('scale-on')
+      })
+    })
 
     this.setState({
       loader: true
-    });
-    
-    this.props.getAllArticles();
-      // .then(() => {
-      //   this.setState({
-      //     loader: false
-      //   });
-      // });
+    })
+
+    this.props.getAllArticles()
+  // .then(() => {
+  //   this.setState({
+  //     loader: false
+  //   })
+  // })
   }
 
-  renderAllArticles() {
-    let allArticles = this.props.articles;
-    if (allArticles.length < 1){
-      return <div className= 'notifier'><h1>No article found </h1></div>
-    }
-
-    else {
-      return allArticles.map((article) =>{
-        return (
-          <Articles 
-          title ={article.blogTitle}
-          post = {article.blogPost}
-          views ={article.views}
-          id={article.id}
-          key={article.id}
-          />
-        )
+  renderAllArticles () {
+    let allArticles = this.props.articles
+    if (allArticles.length < 1) {
+      return <div className='notifier'>
+               <h1>No article found</h1>
+             </div>
+    }else {
+      return allArticles.map((article) => {
+        return ( <main className='all-articles'>
+                   <div className='container mt-4'>
+                     <div className='row'>
+                       <Articles
+                         title={article.blogTitle}
+                         post={article.blogPost}
+                         views={article.views}
+                         id={article.id}
+                         key={article.id} />
+                     </div>
+                   </div>
+                 </main>)
       })
     }
   }
 
-  render() {
+  render () {
     return (
       <div>
-      <div className='all-page'>
-      <NavBar />
-      <div className='row'>
-      {this.renderAllArticles()}
+        <div className='all-page'>
+          <NavBar />
+          {this.renderAllArticles()}
+          <Footer />
+        </div>
       </div>
-      <Footer />
-    </div>
-      </div>
-    );
+    )
   }
 }
 // HomePage.propTypes = {
@@ -86,7 +87,7 @@ class HomePage extends Component {
 const mapStateToProps = (state) => {
   return {
     articles: state.ArticleReducer.articles
-  };
+  }
 }
 
-export default connect(mapStateToProps, {getAllArticles}) (HomePage);
+export default connect(mapStateToProps, {getAllArticles})(HomePage)
